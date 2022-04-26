d37f29b29ef26a6de68f5ce5afd46bc28f58465d
shefali <shefalis2708@gmail.com>
2022-04-26 23:17:21 +0530

    Emp is present or absent

---
+++ EmployeeWageProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EmployeeWageProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Constants
+            int FULL_TIME = 1;
+            Random random = new Random();
+
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
